65243f6bf6a3b6d32d1a76c9831b38e5b162095c
maher ghariani <maherghariani7@gmail.com>
2024-05-14 00:00:44 +0100

    dev:manage user

--- server/Models/userModel.js
+++ server/Models/userModel.js
@@ -1,41 +1,42 @@
 const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
-	name: {
-		type: String,
-		required: true,
-	},
-	surname: {
-		type: String,
-		required: true,
-	},
-	email: {
-		type: String,
-		required: true,
-		unique: true,
-	},
-	password: {
-		type: String,
-		required: true,
-	},
-	avatar: {
-		type: String,
-	},
-	color: {
-		type: String,
-	},
-	boards: [
-		{
-			type: mongoose.Schema.Types.ObjectId,
-			ref: 'board',
-		},
-	],
-	workSpaces: [
-		{
-			type: mongoose.Schema.Types.ObjectId,
-			ref: 'workspace',
-		},
-	],
+  name: {
+    type: String,
+    required: true,
+  },
+  surname: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  avatar: {
+    type: String,
+  },
+  color: {
+    type: String,
+  },
+  role: {
+    type: String,
+    enum: ['user', 'admin'],
+    default: 'user',
+  },
+  boards: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'board',
+  }],
+  workSpaces: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'workspace',
+  }],
 });
 
 module.exports = mongoose.model('user', userSchema);
--- server/Services/userService.js
+++ server/Services/userService.js
@@ -2,11 +2,11 @@ const userModel = require("../Models/userModel");
 const { createRandomHexColor } = require("./helperMethods");
 
 const register = async (user, callback) => {
-  const newUser = userModel({ ...user, color:createRandomHexColor()});
+  const newUser = userModel({ ...user, color: createRandomHexColor() });
   await newUser
     .save()
     .then((result) => {
-      return callback(false, { message: "User created successfuly!" });
+      return callback(false, { message: "User created successfully!" });
     })
     .catch((err) => {
       return callback({ errMessage: "Email already in use!", details: err });
@@ -55,9 +55,54 @@ const getUserWithMail = async (email, callback) => {
   }
 };
 
+const addUser = async (user, callback) => {
+  try {
+    const newUser = userModel({ ...user, color: createRandomHexColor() });
+    await newUser.save();
+    return callback(false, { message: "User added successfully!" });
+  } catch (err) {
+    return callback({ errMessage: "Error adding user!", details: err });
+  }
+};
+
+const deleteUser = async (id, callback) => {
+  try {
+    const result = await userModel.findByIdAndDelete(id);
+    if (!result) return callback({ errMessage: "User not found!" });
+    return callback(false, { message: "User deleted successfully!" });
+  } catch (err) {
+    return callback({ errMessage: "Error deleting user!", details: err });
+  }
+};
+const updateUser = async (id, newData, callback) => {
+  try {
+    const result = await userModel.findByIdAndUpdate(id, newData, { new: true });
+    if (!result) return callback({ errMessage: "User not found!" });
+    return callback(false, { message: "User updated successfully!", updatedUser: result });
+  } catch (err) {
+    return callback({ errMessage: "Error updating user!", details: err });
+  }
+};
+
+
+const getAllUsers = async () => {
+  try {
+    const users = await userModel.find();
+    // Map over the users array to include IDs in each user object
+    const usersWithIds = users.map(user => ({ id: user._id, ...user.toObject() }));
+    return usersWithIds;
+  } catch (error) {
+    console.error('Error fetching all users:', error);
+    throw new Error('Internal server error');
+  }
+};
 module.exports = {
   register,
   login,
   getUser,
   getUserWithMail,
+  addUser,
+  deleteUser,
+  updateUser,
+  getAllUsers,
 };
